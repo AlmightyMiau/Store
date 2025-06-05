31710eb1c486882b85848692b30710e64f7a3c9e
AlmightyMiau <almightymeow612@gmail.com>
2025-06-04 23:24:31 -0700

    Use regular expressions to test login inputs

--- Phase2/login.php
+++ Phase2/login.php
@@ -28,13 +28,38 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </head>
     <body>
         <h2>Login</h2>
-        <form method="POST">
+        <form id="userInput" method="POST">
             Username: <input name="username"><br>
-            Password: <input name="password" type="password"><br>
-            <button type="submit">Login</button>
+            Password: <input name="password"><br>
+            <button id="submitBtn" type="submit">Login</button>
         </form>
         <p style="color:red;"><?php echo $message; ?></p>
         <p><a href="register.php">Create New User</a></p>
         <p><a href="blackjack.html">Play Blackjack</a></p>
     </body>
+    <script>
+        const form = document.getElementById('userInput');
+        const regexUsername = "^[A-Za-z0-9_-]{3,15}$";
+        const regexPassword = "((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%]).{6,20})";
+        
+        const username = form.elements['username'];
+        const password = form.elements['password'];
+
+        function getRegExp(i){
+            return new RegExp(i);
+        }
+        
+        form.addEventListener("submit", function(e) {
+            if ( getRegExp(regexUsername).test(username.value)
+              && getRegExp(regexPassword).test(password.value)
+            ) {
+                return true; // Passes regEx tests, submit to server
+            } else {
+                // Fails regEx tests, alert user and don't submit to server
+                if (e.preventDefault) e.preventDefault(); // Stop the submit maybe
+                form.append("Failed input validation");
+                return false; 
+            }
+        });
+    </script>
 </html>
